c563f368f2b5818a6b31fbda52411782b57a82f3
Chalaya22 <130228713+Chalaya22@users.noreply.github.com>
2023-09-01 07:00:04 +0000

    Deploying to gh-pages from @ Chalaya22/goit-js-hw-09@a24c85229744295a47915263d51db83308e64ac9 🚀

---
+++ 01-color-switcher.d4d8b6f3.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");e.addEventListener("click",(function(e){t.disabled=!1,e.target.disabled=!0,clearInterval(a)})),t.addEventListener("click",(function(t){t.target.disabled=!0,e.disabled=!1,a=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)}));let a=0;
+//# sourceMappingURL=01-color-switcher.d4d8b6f3.js.map
